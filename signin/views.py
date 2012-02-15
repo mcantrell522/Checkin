531778db2b6305b6e0db1ecf00566c68967c80bf
Anthony Sosso <xtremerunnerars@gmail.com>
2012-02-15 03:05:35 -0500

    Added id# association

--- signin/views.py
+++ signin/views.py
@@ -11,6 +11,9 @@ logger = logging.getLogger(__name__)
 
 class SigninForm(forms.Form):
 	idnum = forms.CharField();
+	firstname = forms.CharField();
+	lastname = forms.CharField();
+	associate = forms.BooleanField(required=False)
 	def is_valid(self):
 		return True;
 
@@ -49,18 +52,49 @@ def createNewAdvertisingmethod(request):
 		o.save()
 	return render_to_response('newrecord.html', {'idnum': trimmed_idnum, 'choices': Advertisingmethod.objects.all()}, context_instance=RequestContext(request))
 
+#def old_signin(request):
+#	trimmed_idnum = int(request.POST['idnum'][0:10])
+#	matchCount = len(Member.objects.filter(idnum=trimmed_idnum))
+#	logger.info('signin request for ' + str(trimmed_idnum) + ' matched ' + str(matchCount) + ' members in database')
+#	if matchCount == 1:
+#		# Member already exists, add attendance record
+#		if checkinMember(Member.objects.filter(idnum=trimmed_idnum)[0]):
+#			return render_to_response('signin_success.html', {'': ''}, context_instance=RequestContext(request))
+#		else:
+#			return render_to_response('signin_duplicate.html', {'': ''}, context_instance=RequestContext(request))
+#	else:
+#		return render_to_response('newrecord.html', {'idnum': trimmed_idnum, 'choices': Advertisingmethod.objects.all()}, context_instance=RequestContext(request))
+
 def signin(request):
-	trimmed_idnum = int(request.POST['idnum'][0:10])
-	matchCount = len(Member.objects.filter(idnum=trimmed_idnum))
-	logger.info('signin request for ' + str(trimmed_idnum) + ' matched ' + str(matchCount) + ' members in database')
-	if matchCount == 1:
-		# Member already exists, add attendance record
-		if checkinMember(Member.objects.filter(idnum=trimmed_idnum)[0]):
-			return render_to_response('signin_success.html', {'': ''}, context_instance=RequestContext(request))
-		else:
-			return render_to_response('signin_duplicate.html', {'': ''}, context_instance=RequestContext(request))
-	else:
-		return render_to_response('newrecord.html', {'idnum': trimmed_idnum, 'choices': Advertisingmethod.objects.all()}, context_instance=RequestContext(request))
+	 if request.method == 'POST': # If the form has been submitted...
+	 	form = SigninForm(request.POST) # A form bound to the POST data
+	 	if form.is_valid(): # All validation rules pass
+	 		idnum = form.cleaned_data['idnum']
+	 		needToAssociate = form.cleaned_data['associate']
+	 		if needToAssociate:
+	 			matches = Member.objects.filter(firstname__iexact=form.cleaned_data['firstname'],lastname__iexact=form.cleaned_data['lastname'])
+ 				if len(matches) == 1: #all is well, make the id association
+ 					matches[0].idnum = idnum
+ 					matches[0].save()
+ 				else:
+ 					return render_to_response('newrecord.html', {'idnum': idnum, 'choices': Advertisingmethod.objects.all()}, context_instance=RequestContext(request))
+ 			else:
+ 				matches = Member.objects.filter(idnum=idnum)
+				logger.info('signin request for ' + str(idnum) + ' matched ' + str(len(matches)) + ' members in database')
+			if len(matches) == 1:
+					# Member already exists, add attendance record
+					if checkinMember(matches[0]):
+						return render_to_response('signin_success.html', {'': ''}, context_instance=RequestContext(request))
+					else:
+						return render_to_response('signin_duplicate.html', {'': ''}, context_instance=RequestContext(request))
+			else:
+					return render_to_response('newrecord.html', {'idnum': idnum, 'choices': Advertisingmethod.objects.all()}, context_instance=RequestContext(request))
+	 		#return HttpResponseRedirect('/thanks/') # Redirect after POST
+	 	else:
+	 		form = SigninForm() # An unbound form
+
+	 return render_to_response('contact.html', {'form': form, })
+
 
 def createNewRecord(request):
 	# Create a new record, add attendance record
